a592260f106378d62f1c3bcc709929b6e4571c65
Abel Dimas <avtw37@gmail.com>
2020-01-13 14:51:14 -0800

    built ally function

--- spacegame.py
+++ spacegame.py
@@ -85,6 +85,62 @@ class Enemy(Sprite):
 		self.setheading(random.randint(0, 360))
 
 
+class Ally(Sprite):
+	def __init__(self, spriteshape, color, startx, starty):
+		Sprite.__init__(self, spriteshape, color, startx, starty)
+		self.speed = 8
+		self.setheading(random.randint(0, 360))
+
+	def move(self):
+		self.fd(self.speed)
+
+		# Boundary Detection
+		if self.xcor() > 290:
+			self.setx(290)
+			self.lt(60)
+
+		if self.xcor() < -290:
+			self.setx(-290)
+			self.lt(60)
+
+		if self.ycor() > 290:
+			self.sety(290)
+			self.lt(60)
+
+		if self.ycor() < -290:
+			self.sety(-290)
+			self.lt(60)
+
+
+class Missile(Sprite):
+	def __init__(self, spriteshape, color, startx, starty):
+		Sprite.__init__(self, spriteshape, color, startx, starty)
+		self.shapesize(stretch_wid=0.3, stretch_len=0.4, outline=None)
+		self.speed = 20
+		self.status = "ready"
+		self.goto(-1000, 1000)
+
+	def fire(self):
+		if self.status == "ready":
+			self.goto(player.xcor(), player.ycor())
+			self.setheading(player.heading())
+			self.status = "firing"
+
+	def move(self):
+		if self.status == "ready":
+			self.goto(-1000, 1000)
+
+		if self.status == "firing":
+			self.fd(self.speed)
+
+		# Border Check
+		if self.xcor() < -290 or self.xcor() > 290 or \
+			self.ycor() < -290 or self.ycor() > 290:
+			self.goto(-1000, 1000)
+			self.status = "ready"
+
+
+
 class Game():
 	def __init__(self):
 		self.level = 1
@@ -117,12 +173,15 @@ game.draw_border()
 # Create my sprites
 player = Player("triangle", "white", 0, 0)
 enemy = Enemy("circle", "red", -100, 0)
+missile = Missile("triangle", "yellow", 0, 0)
+ally = Ally("square", "blue", 0, 0)
 
 # Keyboard bindings
 turtle.onkey(player.turn_left, "Left")
 turtle.onkey(player.turn_right, "Right")
 turtle.onkey(player.accelerate, "Up")
 turtle.onkey(player.decelerate, "Down")
+turtle.onkey(missile.fire, "space")
 turtle.listen()
 
 
@@ -130,12 +189,29 @@ turtle.listen()
 while True:
 	player.move()
 	enemy.move()
+	missile.move()
+	ally.move()
 
-	# Check for a collision
+	# Check for a collision with the player
 	if player.is_collision(enemy):
 		x = random.randint(-250, 250)
 		y = random.randint(-250, 250)
 		enemy.goto(x, y)
+
+	# Check for a collision between the missile and the enemy
+	if missile.is_collision(enemy):
+		x = random.randint(-250, 250)
+		y = random.randint(-250, 250)
+		enemy.goto(x, y)
+		missile.status = "ready"
+
+	# Check for a collision between the missile and the ally
+	if missile.is_collision(ally):
+		x = random.randint(-250, 250)
+		y = random.randint(-250, 250)
+		ally.goto(x, y)
+		missile.status = "ready"
+
 
 delay = raw_input("Press enter to finish. > ")
 
